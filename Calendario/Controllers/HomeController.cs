4541abbe450becdc92f77815ddb1c02f1f169f08
Gustavo <gustavo@engsolutions.com.br>
2015-10-15 01:04:51 -0300

    c

---
+++ Calendario/Controllers/HomeController.cs
@@ -0,0 +1,24 @@
+﻿using System.Web.Mvc;
+
+namespace Calendario.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult CalendarMes(int ano,int mes)
+        {
+            return PartialView("_CalendarMes", new[] {ano, mes});
+        }
+
+        public ActionResult Contact()
+        {
+            ViewBag.Message = "Your contact page.";
+
+            return View();
+        }
+    }
+}
